af6626faab74fea891b0896e833e74ec4309196b
Zachary <zacharylampi96@gmail.com>
2022-06-12 16:02:07 -0400

    commit tree ae6fa664d35f6bfe3a790581f10a6da8b9d11426

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if(x<= 400) {
+    return "This one is on me!"
+  } else if (x<= 2000) {
+    return "That will be twenty bucks."
+  }else if (x<2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+  }
+
+function ternaryCheckCity(y){ 
+  const isAdult = (y === "NYC") ? "Ok, sounds good." :  "No go.";
+  return isAdult
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let y="asd"
+  switch (tip) {
+    case 'generous': y='Thank you so much.';
+        break;
+    case 'not as generous': y='Thank you.';
+        break;
+    default: y="Bye.";
+        break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+return y}
